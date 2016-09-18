071f382480c984bb666e6807830cf43bc8c1c3e0
houripj357 <houripj357@gmail.com>
2016-09-17 22:02:05 -0400

    Set up application controller for Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
     include Pundit 
-
+    rescure_from Pundit::NotAuthorizedError, with: :user_not_authorized
     protect_from_forgery with: :exception
 	before_action :configure_permitted_parameters, if: :devise_controller? 
+
+private 
+	def user_not_authorized
+		flash[:alert] = "Access denied."
+		redirect_to (request.referrer || root_path)
+	end
 
 protected 
 	def configure_permitted_parameters
