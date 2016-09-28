551034ce72e8751f40fed0d7649052bad5876afa
houripj357 <houripj357@gmail.com>
2016-09-27 20:18:15 -0400

    update user model for heroku app to function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
 	enum role: [:patron, :artist, :admin]
 	after_initialize :set_default_role, :if => :new_record?
 
-	def set_default_role 
-		self.role ||= :patron
-	end
+	# def set_default_role 
+	# 	self.role ||= :patron
+	# end
 
   	devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :validatable
