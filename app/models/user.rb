37771d47ac1a755c1776fe4fab968aabb9190d79
houripj357 <houripj357@gmail.com>
2016-10-05 22:29:59 -0400

    comment out mailer for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
 	has_and_belongs_to_many :roles
 
-  	devise :database_authenticatable, :registerable, :confirmable,
+  	devise :database_authenticatable, :registerable, 
            :recoverable, :rememberable, :trackable, :validatable
 
     acts_as_messageable 
