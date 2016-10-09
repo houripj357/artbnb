490a3a6a11b41106d8cfcad3f7d16e13ed1f1ef6
patrick-hourihan <houripj357@gmail.com>
2016-10-09 19:19:04 -0400

    update user show profile likes from medium size image to thumbnail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 	end
 
   	def index
-    	@users = User.paginate(page: params[:page], per_page: 8)
+    	@users = User.paginate(page: params[:page], per_page: 20)
     	authorize User
   	end
 
